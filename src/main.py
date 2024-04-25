a964b33ee16521a28c4771dbd4e66b3883a70e59
mawbray <maxmowbray@msn.com>
2024-04-25 16:55:12 +0100

    finally corrected gitignore

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 """
 TODO :
 - samplers module 
-- visualisation module
+- visualisation module - tidy up methods into distinct plot functions and integrate with constructor
 - integration of all code module
 - test and debugging
 - documentation
